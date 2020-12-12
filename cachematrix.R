847a9417cfa96ba95338456835b21c45d49afd8a
jamesvabraham <james.abraham@outlook.com>
2020-12-12 20:28:40 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions use lexical scoping to cache the inverse of matrix
 
-## Write a short comment describing this function
+## How to use
+## cachevector <- makeCacheMatrix - to create list of functions
+## basematrix - the base matrix whose inverse is to be cached)
+## cachevector$set(basematrix) - set the matrix in environment
+## cacheSolve(cachevector) - will solve, cache, and return the inverse
 
-makeCacheMatrix <- function(x = matrix()) {
 
+## The first function creates a special vector of four functions used to
+## cache the inverse of the matrix
+
+makeCacheMatrix <- function(mtrx = matrix()) {
+  inv <- NULL
+  set <- function(y){
+    mtrx <<- y
+    inv <<- NULL
+  }
+  get <- function() mtrx
+  setsolve <- function(nvrs) inv <<- nvrs
+  getsolve <- function() inv
+  list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This second function uses the makeCacheMatrix vector to get the inverse
+## (if already solved) or calculates it if not
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getsolve()
+  if(!is.null(inv)) {
+    message("Getting cached inverse")
+    return(inv)
+  }
+  mtrxdata <- x$get()
+  inv <- solve(mtrxdata)
+  x$setsolve(inv)
+  inv
 }
